686e2525f7e92cba420bad67207a0cdd005ab7c6
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-04-06 19:56:22 +0200

    create Map, add Note and print note, create menu

---
+++ src/main/java/org/example/Control.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Control {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private Map<LocalDate, String> notes = new HashMap<>();
+
+    private void printMenu() {
+        System.out.println("Witaj w swoim dzienniku. Wybierz z opcji poniżej:");
+        System.out.println("1. Opisz swój dzisiejszy dzień:");
+        System.out.println("2. Zobacz co się u ciebie działo w poprzednich dniach");
+    }
+
+    void doMenu(){
+        printMenu();
+        String choice = scanner.nextLine();
+        switch (choice){
+            case "1" -> addNote(LocalDate.now(), "hej");
+            case "2" -> printNote(LocalDate.now());
+            default -> System.out.println("Zły wybór, spróbuj jeszcze raz");
+        }
+    }
+
+
+    private void addNote(LocalDate date, String content) {
+        notes.put(date, content);
+    }
+
+    private String printNote(LocalDate date){
+        return notes.get(date);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        control();
-    }
-
-    static Scanner scanner = new Scanner(System.in);
-
-
-    static void control(){
-        System.out.println("Opisz swój dzisiejszy dzień: ");
-        String progres = scanner.nextLine();
-        System.out.println("Twoje wnioski po dzisejszym dniu: " + progres);
+        Control control = new Control();
+        control.doMenu();
     }
 }
