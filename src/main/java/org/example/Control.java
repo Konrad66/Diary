f80ec9831da9ebc2ffaa1c0fb3c873b356c828ec
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-04-09 23:50:00 +0200

    print notes basic on dates

--- src/main/java/org/example/Control.java
+++ src/main/java/org/example/Control.java
@@ -23,22 +23,32 @@ public class Control {
             String choice = scanner.nextLine();
             switch (choice) {
                 case "1" -> addNote(LocalDate.now());
-                case "2" -> printNote();
+                case "2" -> printNote(LocalDate.now());
                 default -> System.out.println("Zły wybór, spróbuj jeszcze raz");
             }
         }
     }
-
-
-
 
     private void addNote(LocalDate date) {
         String content = scanner.nextLine();
         notes.put(date, content);
     }
 
-    private String printNote(){
+    private String priintNote(LocalDate date){
+        return notes.get(date);
+    }
 
+    private String printNote(LocalDate date){
+        System.out.println("Wpisz datę z jakiego dnia chciałbyś zobaczyć wpis: (RRRR-MM-DD)");
+        String dataString = scanner.nextLine();
+        date = LocalDate.parse(dataString);
+        String showNote = priintNote(date);
+        if (showNote != null){
+            System.out.println("Wpis z dnia " + date + " :");
+            System.out.println(showNote);
+        } else {
+            System.out.println("Nie znaleziono wpisu z tą datą.");
+        }
         return null;
     }
 }
